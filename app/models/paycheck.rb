b75daf735dce64a88f9fb51e9e58bfb00a915e86
Raghu Betina <raghu@firstdraft.com>
2016-12-05 03:26:58 +0000

    Validate presence of paychecks.date_received

--- app/models/paycheck.rb
+++ app/models/paycheck.rb
@@ -5,6 +5,8 @@ class Paycheck < ApplicationRecord
 
   # Validations
 
+  validates :date_received, :presence => true
+
   validates :income_source, :uniqueness => { :scope => [:date_received], :message => "All income received from the same source and on the same date must be summed together into a single total amount from the source on that date" }
 
   validates :income_source, :presence => true
