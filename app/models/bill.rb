2dbd5e49a56768573a61235ed82976691caf96db
Raghu Betina <raghu@firstdraft.com>
2016-12-05 03:26:59 +0000

    Validate uniqueness of bills.amount_due

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -5,6 +5,8 @@ class Bill < ApplicationRecord
 
   # Validations
 
+  validates :amount_due, :uniqueness => { :scope => [:date_due, :recipient], :message => "All bills due to be paid to the same recipient on the same date must be summed together into a single total amount due to the recipient on that date" }
+
   validates :amount_due, :presence => true
 
   validates :amount_due, :numericality => { :greater_than => 0 }
