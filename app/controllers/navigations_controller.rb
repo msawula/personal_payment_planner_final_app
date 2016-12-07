b2c0af2ed862ac539459f6d4824c2041dacc94b1
Michael Sawula <msawula@users.noreply.github.com>
2016-12-06 23:16:22 -0600

    trying...

--- app/controllers/navigations_controller.rb
+++ app/controllers/navigations_controller.rb
@@ -23,9 +23,30 @@ class NavigationsController < ApplicationController
     end
 
     #where my additional code should be
+    @opaychecks = @paychecks.dup
+    @obills = @bills.dup
+    @oprevious_paycheck_date = Date.new(1900,1,1)
+    @opaychecks.each do |opaycheck|
+      @running_total = opaycheck.average_amount
+      @obills.each do |obill|
+        if (obill.date_due > @oprevious_paycheck_date) && (obill.date_due <= opaycheck.date_received)
+          if (obill.amount_due < @running_total)
+            obill.paycheck = opaycheck
+            obill.save
+            @running_total = @running_total - obill.amount_due
+          else
+            obill.paycheck = nil
+          end
+        else
+          obill.paycheck = nil
+        end
+      end
+        @oprevious_paycheck_date = opaycheck.date_received
+    end
 
 
-    #where my additional code should be
+
+    #where my additional code should end
     render("userinteractionpages/timeline.html.erb")
   end
 
