9c2000aaeb78b5550bec486a00f59406fc5fa598
Michael Sawula <msawula@users.noreply.github.com>
2016-12-06 21:33:17 -0600

    timeline is working

--- app/controllers/navigations_controller.rb
+++ app/controllers/navigations_controller.rb
@@ -7,10 +7,27 @@ class NavigationsController < ApplicationController
 
   def timeline
 
-@paychecks = Paycheck.where(:user_id => current_user)
-@bills = Bill.where(:user_id => current_user)
+    @paychecks = Paycheck.where(:user_id => current_user)
+    @bills = Bill.where(:user_id => current_user)
 
+    @previous_paycheck_date = Date.new(1900,1,1)
+    @paychecks.each do |paycheck|
+      @bills.each do |bill|
+        if (bill.date_due > @previous_paycheck_date) && (bill.date_due <= paycheck.date_received)
+          bill.paycheck = paycheck
+          bill.save
+        end
+      end
+      @previous_paycheck_date = paycheck.date_received
+
+    end
+
+    #where my additional code should be
+
+
+    #where my additional code should be
     render("userinteractionpages/timeline.html.erb")
   end
+
 
 end
