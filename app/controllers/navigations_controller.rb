eed4aaaaa317b3da90b911dfc8e351063945c393
Michael Sawula <msawula@users.noreply.github.com>
2016-12-08 20:33:16 -0600

    updated logic

--- app/controllers/navigations_controller.rb
+++ app/controllers/navigations_controller.rb
@@ -7,13 +7,13 @@ class NavigationsController < ApplicationController
 
   def timeline
 
-    @paychecks = Paycheck.where(:user_id => current_user).order(date_received: :asc)
+    @paychecks = Paycheck.where(:user_id => current_user).order(date_received: :desc)
     @bills = Bill.where(:user_id => current_user)
 
-    @previous_paycheck_date = Date.new(1900,1,1)
+    @previous_paycheck_date = Date.new(2099,1,1)
     @paychecks.each do |paycheck|
       @bills.each do |bill|
-        if (bill.date_due > @previous_paycheck_date) && (bill.date_due <= paycheck.date_received)
+        if (bill.date_due < @previous_paycheck_date) && (bill.date_due >= paycheck.date_received)
           bill.paycheck = paycheck
           bill.save
         end
@@ -23,11 +23,12 @@ class NavigationsController < ApplicationController
     end
 
     #where my additional code should be for OPTIMIZED LOGIC
-    @previous_paycheck_date = Date.new(2000,1,1)
+    @previous_paycheck_date = Date.new(2099,1,1)
     @paychecks.each do |paycheck|
       @running_total = paycheck.average_amount
       @bills.each do |bill|
-        if (bill.date_due > @previous_paycheck_date) && (bill.date_due <= paycheck.date_received)
+        puts bill.inspect
+        if (bill.date_due < @previous_paycheck_date) && (bill.date_due >= paycheck.date_received)
           if (bill.amount_due <= @running_total)
             bill.proposed_paycheck = paycheck
             bill.save
